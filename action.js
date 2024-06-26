b233b8e94105dd08c911f4cc808ccd9bd8c9776a
Petter Hoff <petterah@outlook.com>
2024-06-26 12:50:57 +0200

    commit 1

---
+++ action.js
@@ -0,0 +1,8 @@
+document.getElementById("myH1").textContent = "Hello";
+document.getElementById("myP").textContent = "U like pizza!"
+
+
+let age = 25;
+let firstName = "Bro";
+console.log(`your age ${age}`)
+console.log(`your name${firstName}`)
