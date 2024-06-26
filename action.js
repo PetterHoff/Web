e1fe2fb595dc1cd95d3a053ccc54792110a31cd2
Petter Hoff <petterah@outlook.com>
2024-06-26 15:33:32 +0200

    commit 1

--- action.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("myH1").textContent = "Hello";
-document.getElementById("myP").textContent = "U like pizza!"
-
-
-let age = 25;
-let firstName = "Bro";
-console.log(`your age ${age}`)
-console.log(`your name${firstName}`)
---
+++ scripts.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('navbar.html')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.text();
+        })
+        .then(data => {
+            document.getElementById('navbar').innerHTML = data;
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+        });
+});
